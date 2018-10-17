8d55427c89ccd075e2d3864d2141aed7ad7beecb
GuJiuzhen <44224503+GuJiuzhen@users.noreply.github.com>
2018-10-17 16:57:16 +0800

    Add files via upload

---
+++ XGTESTPROGRAM/XGTESTPROGRAM/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XGTESTPROGRAM
+//
+//  Created by 顾九针 on 2018/10/17.
+//  Copyright © 2018 顾九针. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ XGTESTPROGRAM/XGTESTPROGRAMTests/XGTESTPROGRAMTests.m
@@ -0,0 +1,37 @@
+//
+//  XGTESTPROGRAMTests.m
+//  XGTESTPROGRAMTests
+//
+//  Created by 顾九针 on 2018/10/17.
+//  Copyright © 2018 顾九针. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XGTESTPROGRAMTests : XCTestCase
+
+@end
+
+@implementation XGTESTPROGRAMTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
